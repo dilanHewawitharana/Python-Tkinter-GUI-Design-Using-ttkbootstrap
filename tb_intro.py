2825ac479a299e31a9091e8abab23af61819aba6
Dilan Sarith Hewawitharana <dshewawitharana@gmail.com>
2024-01-08 23:02:10 +0900

    Basic setup

---
+++ tb_intro.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from ttkbootstrap.constants import *
+import ttkbootstrap as tb
+
+root = tb.Window(themename='superhero')
+
+root.title('TTK Bootstrap!')
+root.iconbitmap('images/icon.png')
+root.geometry('500x350')
+
+root.mainloop()
